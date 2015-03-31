a417bc6fd4896b3ffc6bc30b95c1f950a5ab0042
stijnlaurent <stijn.laurent@telenet.be>
2015-03-31 08:08:35 +0200

    even getest met nieuwe pin

--- TestGit.ino
+++ TestGit.ino
@@ -1,13 +1,13 @@
 void setup()
 {
-
+	int LedPin = 13;
   /* add setup code here */
-
+	pinMode(LedPin, OUTPUT);
 }
 
 void loop()
 {
-
+	digitalWrite(LedPin, HIGH);
   /* add main program code here */
 
 }

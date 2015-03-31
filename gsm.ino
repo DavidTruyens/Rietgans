b62c6397a5b0bf4d661167e5c7d88b2ca773bcbb
stijnlaurent <stijn.laurent@telenet.be>
2015-03-31 08:34:02 +0200

    test

--- gsm.ino
+++ gsm.ino
@@ -1,1 +1,15 @@
+void setup()
+{
+	int LedPin = 13;
+	/* add setup code here */
+	pinMode(LedPin, OUTPUT);
+}
+
+void loop()
+{
+	digitalWrite(LedPin, HIGH);
+
+	/* add main program code here */
+
+}
 

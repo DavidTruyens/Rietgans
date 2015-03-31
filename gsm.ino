58f69e4d16d695d5bea59136a770576f94ec00f4
stijnlaurent <stijnlaurent@users.noreply.github.com>
2015-03-31 09:08:45 +0200

    Update gsm.ino

--- gsm.ino
+++ gsm.ino
@@ -1,15 +1,12 @@
+int LedPin = 13;
+
 void setup()
 {
-	int LedPin = 13;
-	/* add setup code here */
 	pinMode(LedPin, OUTPUT);
 }
 
 void loop()
 {
 	digitalWrite(LedPin, HIGH);
-
-	/* add main program code here */
-
 }
 

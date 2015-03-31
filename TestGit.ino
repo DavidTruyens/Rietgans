6ff43a7666c3da9aff10e84bb646e9ee8a5a610b
stijnlaurent <stijn.laurent@telenet.be>
2015-03-31 09:16:35 +0200

    Pin 3 ipv 13

--- TestGit.ino
+++ TestGit.ino
@@ -1,14 +1,11 @@
+int LedPin = 3;
+
 void setup()
 {
-	int LedPin = 13;
-  /* add setup code here */
 	pinMode(LedPin, OUTPUT);
 }
 
 void loop()
 {
 	digitalWrite(LedPin, HIGH);
-	
-  /* add main program code here */
-
 }
